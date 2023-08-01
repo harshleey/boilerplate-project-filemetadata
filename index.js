0312097b4ea1a8baf00145ab5707d02c5fa52b1f
Wonuola <logun.fateem@gmail.com>
2023-08-01 10:03:07 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +18,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res) => {
+  const { originalname, mimetype, size} = req.file
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
